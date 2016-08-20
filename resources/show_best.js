93bea41de089914470dda1015c8a83bf16da85b6
Areeb Beigh <areebbeigh@gmail.com>
2016-08-20 22:02:25 +0530

    Added database (record keeping) and improved HTML display
    Being able to compare the typing statistics with previous one is a cool
    feature so I decided to add it. To reset a database simply delete the
    database folder or the .db file in it.

---
+++ resources/show_best.js
@@ -0,0 +1,39 @@
+// Author: Areeb Beigh <areebbeigh@gmail.com>
+// Not the best of scripts in JS but this does the thing
+
+var showBest = function(id) {
+    var nodes = document.querySelectorAll(id);
+    var valueNode = {};
+    var nodeValues = [];
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        var value = node.textContent;
+        valueNode[Number(value)] = node;
+        nodeValues.push(Number(value))
+    }
+    if (id != "#wrong") {
+        var greatestValue = 0;
+        for (var i = 0; i < nodeValues.length; i++) {
+            if (nodeValues[i] > greatestValue)
+                greatestValue = nodeValues[i];
+        }
+        var res = valueNode[greatestValue];
+    }
+    else {
+        // the best entry in wrong is the one that is the minimum
+        var smallestValue = 9999999999999999;
+        for (var i = 0; i < nodeValues.length; i++) {
+            if (nodeValues[i] < smallestValue)
+                smallestValue = nodeValues[i];
+        }
+        var res = valueNode[smallestValue];
+    }
+    res.style = "border: 2px solid red;"
+};
+
+var showBestAll = function() {
+     showBest("#accuracy");
+     showBest("#speed");
+     showBest("#right");
+     showBest("#wrong");
+}
