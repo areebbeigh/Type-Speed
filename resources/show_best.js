96646de452ca8c98276f0f468f7458c614301e5d
Areeb Beigh <areebbeigh@gmail.com>
2016-09-09 19:03:26 +0530

    Fixing repo

---
+++ resources/show_best.js
@@ -0,0 +1,37 @@
+// Author: Areeb Beigh <areebbeigh@gmail.com>
+// Not the best of scripts in JS but this gets the job done
+
+var showBest = function(id) {
+    var nodes = document.querySelectorAll(id);
+    var valueNode = {};
+    var nodeValues = [];
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        var value = node.textContent;
+        valueNode[Number(value)] = node;
+        nodeValues.push(Number(value))
+    }
+    if (id != "#wrong") {
+        var greatestValue = 0;
+        for (var i = 0; i < nodeValues.length; i++) {
+            if (nodeValues[i] > greatestValue)
+                greatestValue = nodeValues[i];
+        }
+        var res = valueNode[greatestValue];
+    }
+    else {
+        // the best entry in wrong is the one that is the minimum
+        var smallestValue = 9999999999999999;
+        for (var i = 0; i < nodeValues.length; i++) {
+            if (nodeValues[i] < smallestValue)
+                smallestValue = nodeValues[i];
+        }
+        var res = valueNode[smallestValue];
+    }
+    res.style = "border: 2px solid red;"
+};
+
+var showBestAll = function() {
+    elements = ["#accuracy", "#speed", "#right", "#wrong"]
+    elements.forEach(showBest)
+}
---
+++ src/managedb.py
@@ -0,0 +1,146 @@
+"""
+Manages database read/write
+"""
+
+import os
+import shutil
+import sqlite3
+import webbrowser
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.abspath(os.path.join(BASE_DIR, 'database', 'typespeed_data.db'))
+HTML_FILE = os.path.join(BASE_DIR, "records.html")
+TABLE_NAME = "TYPESPEED_RECORDS"
+
+
+class ManageDB:
+    def __init__(self):
+        if not os.path.exists(DB_PATH):
+            self.create_database()
+
+        self.conn = sqlite3.connect(DB_PATH)
+        self.c = self.conn.cursor()
+
+    def __del__(self):
+        # Close the connection to the db
+        self.conn.close()
+
+    def create_database(self):
+        """
+        Creates a new database at `DB_PATH` and the table `TABLE_NAME` in it
+        """
+
+        directory = os.path.split(DB_PATH)[0]
+
+        # Remove the database file if it already exists
+        if os.path.exists(DB_PATH):
+            shutil.rmtree(directory)
+
+        os.mkdir(directory)
+
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("""CREATE TABLE %s (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                entry_date TEXT NOT NULL,
+                right_words INT NOT NULL,
+                wrong_words INT NOT NULL,
+                accuracy REAL NOT NULL,
+                speed REAL NOT NULL)""" % TABLE_NAME)
+        conn.commit()
+
+    def save_data(self, data):
+        """
+        Saves the given dictionary data to the database
+
+        Parameters:
+            data - A dictionary that contains the data to be saved in the database
+        """
+
+        entry_date = datetime.now().strftime("%d/%m/%y %I:%M:%S %p")
+        right_words = int(data['right'])
+        wrong_words = int(data['wrong'])
+        accuracy = float(data['accuracy'])
+        speed = float(data['speed'])
+
+        if not os.path.exists(DB_PATH):
+            self.create_database()
+
+        self.c.execute(
+            """INSERT INTO %s (entry_date, right_words, wrong_words, accuracy, speed)
+            VALUES (?, ?, ?, ?, ?)
+            """ % TABLE_NAME,
+            (
+                entry_date,
+                right_words,
+                wrong_words,
+                accuracy,
+                speed,
+            )
+        )
+        self.conn.commit()
+
+    def view_records(self):
+        """
+        Generates an HTML file from the database records and opens it
+        in the default browser
+        """
+
+        records = self.c.execute(
+            "SELECT entry_date, right_words, wrong_words, accuracy, speed FROM %s ORDER BY id desc" % TABLE_NAME)
+
+        html = \
+            """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Type Speed Records</title>
+    <link rel="stylesheet" href="resources/stylesheet.css" type="text/css">
+    <script src="resources/show_best.js" type="text/javascript"></script>
+</head>
+<body>
+    <div id="wrapper">
+        <h1>Type Speed Records</h1>
+        <h2><a href="https://github.io/areeb-beigh">github.io/areeb-beigh</a></h2>
+        <div id="box">
+            <table width="30%" border="0" cellpadding="10px" cellspacing="0px">
+                <tr>
+                    <th>Date</th>
+                    <th>Right Words</th>
+                    <th>Wrong Words</th>
+                    <th>Accuracy</th>
+                    <th>Speed</th>
+                </tr>
+        """
+
+        for entry in records:
+            entry = list(map(str, entry))
+            row = \
+                """
+                <tr>
+                    <td id="date">%s</td>
+                    <td id="right">%s</td>
+                    <td id="wrong">%s</td>
+                    <td id="accuracy">%s</td>
+                    <td id="speed">%s</td>
+                </tr>
+                """ % \
+                (
+                    entry[0],
+                    entry[1],
+                    entry[2],
+                    entry[3],
+                    entry[4]
+                )
+            html += row
+
+        html += """</table></div>
+        <input id="showButton" type="button" onclick="showBestAll()" value="Show Best">
+        </div></body></html>"""
+
+        with open(HTML_FILE, "w") as f:
+            f.write(html)
+
+        webbrowser.open(HTML_FILE)
